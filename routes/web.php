914254c9582aa912cf85896fb90babacfdff3ed8
Marcelo V. Almeida <marceloalmeidaengenharia@gmail.com>
2022-07-04 19:50:42 -0300

    UPDATE [web.php]
    Updated web.php . its cleaning comments and others features from class Laravel.

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello-world', function () {
-    echo '<h1>hello world!</h1>';
-});
-
-// Route::get('/users/{nome}', function ($nome) {
-//     echo "<h1>$nome</h1>";
-// });
-
-// php artisan make:controller NomeDoController  (para criar controllers)
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
-//Via Cep Web Service
-Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
-Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
-Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
+
 
 
