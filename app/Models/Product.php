f0f0bec718a1804cdc895e4e7d4b9e388d5b6adf
Avilla Gabriella <avillagabriella@outlook.com>
2022-07-07 17:39:35 -0300

    refactor adjusting model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Produto extends Model
+class Product extends Model
 {
     protected $table = 'products';
     use HasFactory;
