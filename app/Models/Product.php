47167d715f8d0d3629e5107eef2e7ad416695e22
Avilla Gabriella <avillagabriella@outlook.com>
2022-07-07 17:20:08 -0300

    feat model product created

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    protected $table = 'products';
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'amount', 'cost_price', 'sale_price', 'main_photo'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+}
