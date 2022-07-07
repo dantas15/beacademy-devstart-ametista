bc16bf011824f1ca89e09771e18e366dde050713
Avilla Gabriella <avillagabriella@outlook.com>
2022-07-07 16:30:02 -0300

    chore add migrate product and category

---
+++ database/migrations/2022_07_04_184828_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->integer('quantidade');
+            $table->float('preco_de_custo', 8, 2);
+            $table->float('preco_de_venda', 8, 2);
+            $table->string('foto_principal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
