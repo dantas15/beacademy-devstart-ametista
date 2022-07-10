0f57a68cde3114269e8ce3d1020642ae0553ccfe
Gustavo Galote <gusgalot3g@gmail.com>
2022-07-10 12:45:04 -0300

    feat: add basic i18n for validation

---
+++ lang/pt-br/validation.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'regex' => 'O formato do :attribute é invalido.',
+    'required' => 'O campo :attribute é obrigatório.',
+
+    'attributes' => [
+        'name' => 'nome',
+        'password' => 'senha',
+        'phone_number' => 'número de celular',
+        'birth_date' => 'data de nascimento',
+        'document_id' => 'documento',
+
+        'zip' => 'cep',
+        'city' => 'cidade',
+        'street' => 'rua',
+        'number' => 'número',
+        'neighborhood' => 'bairro',
+        'complement' => 'complemento',
+    ],
+];
