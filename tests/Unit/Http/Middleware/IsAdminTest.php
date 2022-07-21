d12964156aa1011096422686f830294726cbd028
Gustavo Galote <gusgalot3g@gmail.com>
2022-07-21 00:07:19 -0300

    test(admin): test IsAdmin middleware

---
+++ tests/Unit/Http/Middleware/IsAdminTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IsAdminTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Regular user should be redirected to index.
+     *
+     * @return void
+     */
+    public function test_normal_user_accessing_admin_route_should_be_redirected_to_index()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/admin');
+
+        $response->assertRedirect('/');
+    }
+
+    /**
+     * Admin should be able to pass the middleware
+     *
+     * @return void
+     */
+    public function test_admin_should_be_able_to_enter_admin_page()
+    {
+        $user = User::factory()->create(['is_admin' => 1]);
+
+        $response = $this->actingAs($user)->get('/admin');
+
+        $response->assertViewIs('admin.index');
+    }
+}
