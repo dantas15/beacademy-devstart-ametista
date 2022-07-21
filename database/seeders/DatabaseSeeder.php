5ced5339c6ea8266ca444a8b4ab060523b124916
Gustavo Galote <gusgalot3g@gmail.com>
2022-07-20 23:48:23 -0300

    feat(seeder): add User and Address seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class, // 1 Admin, 2 User w/o Addresses
+            AddressSeeder::class, // Admin with address
         ]);
     }
 }
