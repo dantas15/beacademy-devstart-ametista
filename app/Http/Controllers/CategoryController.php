b8207f06dc9b20e7390398ea13aec8d62aa42325
Gustavo Galote <gusgalot3g@gmail.com>
2022-07-28 12:27:42 -0300

    style: update PHPDocblock and code style

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Http\Response;
 
 class CategoryController extends Controller
 {
-
-    public function index(){
+    /**
+     * Show categories index page.
+     *
+     * @return View
+     */
+    public function index()
+    {
         $categories = Category::orderBy('id', 'desc')->paginate(5);
         return view('categories.index', compact('categories'));
     }
@@ -16,57 +24,49 @@ class CategoryController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
-    //public function create(Request $dados)
     public function create()
     {
-        //  echo"create";exit;
         return view('categories.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
-
-        //echo '<pre>';print_r(request()->get('quantidade'));exit;
-
         $request->validate([
             'name' => 'required'
         ]);
 
-        $category= new Category;
+        $category = new Category;
         $category->name = $request->name;
 
         $category->save();
 
         return redirect()->route('admin.categories.index')->with('success', 'Categoria criada com sucesso.');
-
-
-
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
-    public function show($id)
-    {
-        //
-    }
+//    public function show($id)
+//    {
+//        //
+//    }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return View
      */
     public function edit(Category $category)
     {
@@ -76,11 +76,11 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id)
     {
 
         $request->validate([
@@ -90,20 +90,18 @@ class CategoryController extends Controller
         $category = Category::find($id);
         $category->name = $request->name;
         $category->save();
-        return redirect()->route('admin.categories.index')
-            ->with('success', 'category atualizado com sucesso');
+        return redirect()->route('admin.categories.index')->with('success', 'category atualizado com sucesso');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return RedirectResponse
      */
     public function destroy(Category $category)
     {
         $category->delete();
-        return redirect()->route('admin.categories.index')
-            ->with('success', 'category deletado com sucesso');
+        return redirect()->route('admin.categories.index')->with('success', 'category deletado com sucesso');
     }
 }
