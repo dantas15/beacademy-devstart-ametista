b62fa51b9e09c573ec4257fbebde6214390ea19b
Avilla Gabriella <avillagabriella@outlook.com>
2022-07-31 19:21:51 -0300

    chore:add SoftDeletes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-   
     protected $table = 'products';
     use HasFactory, SoftDeletes;
 
