cb51e58a010b0f361681b2245b5b850d92c97cfe
Avilla Gabriella <avillagabriella@outlook.com>
2022-08-01 18:46:55 -0300

    refactor:adjusting controller products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::orderBy('id', 'desc')->paginate(10);
+        $products = Product::orderBy('id', 'desc')->paginate(5);
         return view('products.index', compact('products'));
     }
 
